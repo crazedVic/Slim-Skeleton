caa662f16e0e43f2cb81f42152746bbda11c3cc2
Vic Rubba <crazedVic@users.noreply.github.com>
2016-06-05 15:45:14 -0600

    Update settings.php

--- src/settings.php
+++ src/settings.php
@@ -1,17 +1,3 @@
 <?php
-return [
-    'settings' => [
-        'displayErrorDetails' => true, // set to false in production
 
-        // Renderer settings
-        'renderer' => [
-            'template_path' => __DIR__ . '/../templates/',
-        ],
 
-        // Monolog settings
-        'logger' => [
-            'name' => 'slim-app',
-            'path' => __DIR__ . '/../logs/app.log',
-        ],
-    ],
-];
